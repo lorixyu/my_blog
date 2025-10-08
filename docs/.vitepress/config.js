575c4cbdf98bc4aabddc9d8cef4ea695216a5a7c
lorixyu <lorixyu@outlook.com>
2025-10-09 06:55:07 +0800

    add vitepress config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'My Blog',
+  description: 'A personal blog powered by VitePress',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Notes', link: '/note' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Home', link: '/' },
+          { text: 'My Note', link: '/note' }
+        ]
+      }
+    ]
+  }
+}
+
