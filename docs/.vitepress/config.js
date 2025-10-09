0eb5948d86f9e9b4ce5591e04c350d1213ea48ed
lorixyu <lorixyu@outlook.com>
2025-10-10 06:36:38 +0800

    new config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,27 +1,30 @@
 export default {
-  title: 'lorixyu\'s blog',
-  titleTemplate: false,
-  description: 'A personal blog powered by VitePress',
+  title: "LorixYu's Blog",
+  description: "A personal blog powered by VitePress",
 
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Notes', link: '/note' }
     ],
-    outline: {
-      level: [1, 3],
-      label: 'On this page'
-    },  // ← 加上这个逗号！！！
 
     sidebar: [
       {
         text: 'Guide',
         items: [
           { text: 'Home', link: '/' },
-          { text: 'My Note', link: '/note' },
-	  { text: '实现myfunction', link: '/function' },
+          {
+            text: 'C++',  // 父级标题
+            collapsible: true,   // 允许折叠
+            collapsed: true,    // 折叠
+            items: [
+              { text: 'My Note', link: '/note' },
+              { text: '实现myfunction', link: '/function' }
+            ]
+          }
         ]
       }
     ]
   }
 }
+
